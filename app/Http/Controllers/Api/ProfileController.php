3c00ed811273c8d8487bf44c19c6d54b2ac7adb4
Maha Elshawardy <108660346+MahaElshawardy@users.noreply.github.com>
2025-02-18 10:38:22 +0200

    Update ProfileController.php

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\UpdatePasswordRequest;
-use App\Http\Requests\Api\UpdateProfileRequest;
-use App\Http\Resources\Api\UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\UserResource;
+use App\Http\Requests\Api\UpdateProfileRequest;
+use App\Http\Requests\Api\UpdatePasswordRequest;
 
 class ProfileController extends Controller
 {
@@ -18,7 +19,11 @@ class ProfileController extends Controller
     public function updateProfile(UpdateProfileRequest $request)
     {
         $data = $request->validated();
-        $user = auth()->user()->update($data);
+        $user = auth()->user();
+
+        DB::transaction(function () use ($user, $data) {
+            $user->update($data);
+        });
         return $this->success("Successfully", new UserResource($user));
     }
     public function updatePassword(UpdatePasswordRequest $request)
