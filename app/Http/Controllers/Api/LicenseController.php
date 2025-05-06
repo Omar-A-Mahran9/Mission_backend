be4c3c0ec830e0809399db7b5681456a61d11240
Maha Elshawardy <108660346+MahaElshawardy@users.noreply.github.com>
2025-05-06 10:42:57 +0300

    modified

---
+++ app/Http/Controllers/Api/LicenseController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreDocumentRequest;
+use App\Http\Requests\Api\UpdateDocumentRequest;
+use App\Http\Resources\Api\DocumentResource;
+use App\Services\Api\CertificateService;
+use App\Services\Api\LicenseService;
+use Illuminate\Http\Request;
+
+class LicenseController extends Controller
+{
+    protected $service;
+
+    public function __construct(LicenseService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        return $this->success("", DocumentResource::collection($this->service->index()));
+    }
+
+    public function store(StoreDocumentRequest $request)
+    {
+        $created = $this->service->store($request);
+        if (!$created) {
+            return $this->failure(__('Error in creating certificate'));
+        }
+        return $this->success('', new DocumentResource($created));
+    }
+    public function update(UpdateDocumentRequest $request, $id)
+    {
+        $updated = $this->service->update($request, $id);
+        if (!$updated) {
+            // return $this->failure(__('Error in updating certificate'));
+            return $this->failure($updated);
+        }
+        return $this->success('', new DocumentResource($updated));
+    }
+    public function destroy($id)
+    {
+        $deleted = $this->service->destroy($id);
+        if (!$deleted) {
+            return $this->failure(__('Error in deleting certificate'));
+        }
+        return $this->success("", DocumentResource::collection($deleted));
+    }
+}
---
+++ app/Repositories/Api/Eloquent/LicenseRepository.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Repositories\Api\Eloquent;
+
+use Illuminate\Support\Facades\DB;
+use App\Repositories\Api\Contracts\LicenseRepositoryInterface;
+
+class LicenseRepository implements LicenseRepositoryInterface
+{
+    public function index()
+    {
+        return auth()->user()->Licenses()
+            ->with('files') // eager load files for performance
+            ->get();
+    }
+
+    public function store($data)
+    {
+        try {
+            DB::transaction(function () use ($data) {
+                $certificate = auth()->user()->Licenses()->create([
+                    'name' => $data['name'],
+                    'type_id' => 2,
+                    'expiration_date' => $data['expiration_date'],
+                    'have_expiration_date' => $data['have_expiration_date']
+                ]);
+                foreach ($data['files'] as $file) {
+                    $certificate->files()->create(['file' => uploadImageToDirectory($file, "Documents")]);
+                }
+            });
+            return auth()->user()
+                ->Licenses()
+                ->with(['files'])
+                ->latest()
+                ->first();
+        } catch (\Throwable $e) {
+            return false;
+        }
+    }
+
+    public function update($data, $id)
+    {
+        try {
+            $certificate = auth()->user()->Licenses()->with("files")->find($id);
+            if (!$certificate) {
+                return false;
+            }
+            $deletedFileIds = $data['deleted_files'] ?? [];
+            $matchedFiles = $certificate->files()->whereIn('id', $deletedFileIds)->get();
+            if (count($matchedFiles) !== count($deletedFileIds)) {
+                return false;
+            }
+            DB::transaction(function () use ($data, $certificate, $matchedFiles) {
+
+                $certificate->update([
+                    'name' => $data['name'],
+                    'expiration_date' => $data['expiration_date'] ?? null,
+                    'have_expiration_date' => $data['have_expiration_date']
+                ]);
+
+                if ($matchedFiles) {
+                    deleteImagesFromDirectory($matchedFiles->pluck('file'), "Documents");
+                    $matchedFiles->each->delete();
+                }
+                if (isset($data['files'])) {
+                    foreach ($data['files'] as $file) {
+                        $certificate->files()->create(['file' => uploadImageToDirectory($file, "Documents")]);
+                    }
+                }
+            });
+            return $certificate->load('files');
+        } catch (\Throwable $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $certificate = auth()->user()->Licenses()->find($id);
+            if (!$certificate) {
+                return false;
+            }
+            DB::transaction(function () use ($certificate) {
+                $certificate->files()->each(function ($file) {
+                    deleteImageFromDirectory($file->file, "Documents");
+                    $file->delete();
+                });
+                $certificate->delete();
+            });
+            return auth()->user()
+                ->Licenses()
+                ->with(['files'])
+                ->get();
+        } catch (\Throwable $e) {
+            return false;
+        }
+    }
+}
